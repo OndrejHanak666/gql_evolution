33068a4c971cbb2201c654f5bbc9ca0885698a53
Bederig Simp <bedrig@nasilnik.tvymamy>
2026-01-05 18:05:16 +0100

    Merge stashed changes: Add publication mutations

--- src/GraphTypeDefinitions/mutation.py
+++ src/GraphTypeDefinitions/mutation.py
@@ -4,8 +4,17 @@ from .EventGQLModel import EventMutation
 from .EventInvitationGQLModel import EventInvitationMutation
 from .PublicationGQLModel import PublicationMutation
 from .PublicationAuthorGQLModel import PublicationAuthorMutation
+from .PublicationTypeGQLModel import PublicationTypeMutation
+from .PublicationCategoryGQLModel import PublicationCategoryMutation
 
 @strawberry.type(description="""Type for mutation root""")
-class Mutation(EventMutation, EventInvitationMutation, PublicationMutation, PublicationAuthorMutation):
+class Mutation(
+    EventMutation, 
+    EventInvitationMutation,
+    PublicationMutation,
+    PublicationAuthorMutation,
+    PublicationTypeMutation,
+    PublicationCategoryMutation
+):
     pass
 
